2b2e06afa22117fcf4010e4ec6f24c3627a4957f
Trey Bertram <treybertram06@gmail.com>
2025-07-17 10:55:44 -0700

    Add platform-specific handling for framebuffer and texture creation on macOS
    Implemented `glGenFramebuffers` and `glGenTextures` for macOS while maintaining `glCreateFramebuffers` and `glCreateTextures` for Windows/Linux. Adjusted texture parameters for depth attachments on macOS to ensure compatibility.

--- engine/src/platform/opengl/opengl_framebuffer.cpp
+++ engine/src/platform/opengl/opengl_framebuffer.cpp
@@ -23,10 +23,20 @@ namespace Honey {
 	        glDeleteTextures(1, &m_depth_attachment);
 	    }
 
+#if defined(HN_PLATFORM_WINDOWS) || defined(HN_PLATFORM_LINUX)
 		glCreateFramebuffers(1, &m_renderer_id);
+#endif
+#if defined(HN_PLATFORM_MACOS)
+		glGenFramebuffers(1, &m_renderer_id);
+#endif
 		glBindFramebuffer(GL_FRAMEBUFFER, m_renderer_id);
 
+#if defined(HN_PLATFORM_WINDOWS) || defined(HN_PLATFORM_LINUX)
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_color_attachment);
+#endif
+#if defined(HN_PLATFORM_MACOS)
+		glGenTextures(1, &m_color_attachment);
+#endif
 		glBindTexture(GL_TEXTURE_2D, m_color_attachment);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_specification.width, m_specification.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -34,10 +44,25 @@ namespace Honey {
 
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_color_attachment, 0);
 
+#if defined(HN_PLATFORM_WINDOWS) || defined(HN_PLATFORM_LINUX)
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_depth_attachment);
 		glBindTexture(GL_TEXTURE_2D, m_depth_attachment);
 		glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH24_STENCIL8, m_specification.width, m_specification.height);
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_depth_attachment, 0);
+#endif
+#if defined(HN_PLATFORM_MACOS)
+		glGenTextures(1, &m_depth_attachment);
+		glBindTexture(GL_TEXTURE_2D, m_depth_attachment);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_specification.width, m_specification.height, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, nullptr);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_depth_attachment, 0);
+#endif
 
 		HN_CORE_ASSERT(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE, "Framebuffer is incomplete!");
 
